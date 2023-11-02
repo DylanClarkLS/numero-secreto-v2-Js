02b12044abdb3df303f07dd55244af2b14816ca4
DylanClarkLS <dylanclarklopes@gmail.com>
2023-11-02 13:17:16 -0300

    Cria uma função com retorno e exibi o número aleatorio no console

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ titulo.innerHTML = 'Jogo do número secreto';
 let paragrafo = document.querySelector('p');
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10'; */
 
+let numeroSecreto = gerarNumeroSecreto();
+
 function exibirTextoNaTela(tag, texto){
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
@@ -12,6 +14,10 @@ function exibirTextoNaTela(tag, texto){
 exibirTextoNaTela('h1', 'Jogo do número secreto');
 exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
+function gerarNumeroSecreto() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
 function verificarChute(){
-    console.log('Botão clicado!');
+    console.log(numeroSecreto);
 }
